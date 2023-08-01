6649e7bd25895286f2d53441d19520737d86679e
EcossB <erwingcoss21@hotmail.com>
2023-08-01 12:13:28 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JOptionPane frame = new JOptionPane();
+
+        Object[] options = {"Conversor Monedas", "Conversor Temperatura"}; // Array que contiene las opciones del menu principal
+
+        String s = (String)JOptionPane.showInputDialog(frame,"Que desea hacer?", "Menu", JOptionPane.PLAIN_MESSAGE, null, options, "Escoge");
+
+        System.out.println(s);
+
+        if(s == "Conversor Monedas"){
+            System.out.println("estas en las opciones del menu");
+        }
+
+    }
+}
