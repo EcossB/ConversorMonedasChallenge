c54f1bf66e908bccd57c310a8cb33b070f360d35
EcossB <erwingcoss21@hotmail.com>
2023-08-02 12:53:08 -0400

    Delete the void and creating a input menu on the construct

--- src/ClaseConversor.java
+++ src/ClaseConversor.java
@@ -2,12 +2,9 @@ import javax.swing.*;
 public class ClaseConversor {
 
     public ClaseConversor(){
-
+        JFrame frame = new JFrame();
+        String num = JOptionPane.showInputDialog(frame, "Ingresa la cantidad de dinero que desea convertir");
+        System.out.println(num);
     }
 
-    public void Input(String s){
-        if(s.equals("Conversor Monedas")){
-
-        }
-    }
 }
