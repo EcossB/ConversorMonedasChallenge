401cf9408bac52a3d1030f69e20808f6989c6634
EcossB <erwingcoss21@hotmail.com>
2023-08-02 13:58:55 -0400

    Creando Documentacion del codigo

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,21 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-
         MenuPrincipal();
     }
 
+
+    /**
+     * Este metodo contiene el menu principal, lo que este metodo hace es validar que opcion quiere el usuario.
+     * Digase si quiere convertir monedas a otras divisas o si quiere hacer conversiones de temperatura.
+     * Dentro de este metodo se hace tambien manejo de errores a NullPointer, si ocurre un error de este tipo
+     * se agarra la excepcion y se lanza un cuadro de dialogo diciendo que el usuario presiono cancelar y se termina el programa.
+     */
     private static void MenuPrincipal(){
-        JOptionPane frame = new JOptionPane();
+        JOptionPane frame = new JOptionPane(); // instanciando la clase JOptionPane
 
         Object[] options = {"Conversor Monedas", "Conversor Temperatura"}; // Array que contiene las opciones del menu principal
-
-        String s = (String)JOptionPane.showInputDialog(frame,"Que desea hacer?", "Menu", JOptionPane.PLAIN_MESSAGE, null, options, "Escoge");
-
+        String s = (String)JOptionPane.showInputDialog(frame,"Que desea hacer?", "Menu", JOptionPane.PLAIN_MESSAGE, null, options, "Escoge"); // Creando cuadro de texto con las diferentes opciones
         System.out.println(s);
 
         try{
