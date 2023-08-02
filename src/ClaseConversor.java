914657fe83d7da14b5101d3359a0fed4ae1385e3
EcossB <erwingcoss21@hotmail.com>
2023-08-02 13:09:05 -0400

    Haciendo manejo de excepciones para nullpointer y NumberFormat

--- src/ClaseConversor.java
+++ src/ClaseConversor.java
@@ -1,10 +1,20 @@
 import javax.swing.*;
+
+import static com.sun.jmx.mbeanserver.Util.cast;
+
 public class ClaseConversor {
 
     public ClaseConversor(){
         JFrame frame = new JFrame();
-        String num = JOptionPane.showInputDialog(frame, "Ingresa la cantidad de dinero que desea convertir");
-        System.out.println(num);
+        String num1 = JOptionPane.showInputDialog(frame, "Ingresa la cantidad de dinero que desea convertir");
+
+        try{
+            int num = Integer.parseInt(num1);
+            System.out.println(num);
+        } catch(NullPointerException | NumberFormatException e){
+            e.printStackTrace();
+        }
+
     }
 
 }
