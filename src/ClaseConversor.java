a8fdce34c09b0914480575d1dc5404f427a55f72
EcossB <erwingcoss21@hotmail.com>
2023-08-05 11:45:40 -0400

    Generando javadoc

--- src/ClaseConversor.java
+++ src/ClaseConversor.java
@@ -7,12 +7,20 @@ public class ClaseConversor {
         Input(frame);
     }
 
-
+    /**
+     * Este metodo de input recibe un parametro tipo JFrame para crear un cuadro de dialogo que acepatara el input del usuario
+     * al recibir el input del usuario, este mismo metodo llama a otro llamado seleccion de divisa para que el usuario escoja.
+     * si el usuario ingresa un String el programa lanzara una excepcion y un cuadro de dialogo diciendo que ingrese un numero
+     * y se termina el programa como castigo.
+     * @throws NullPointerException
+     * @throws NumberFormatException
+     * @param frame
+     */
     public static void Input(JFrame frame){
         String num1 = JOptionPane.showInputDialog(frame, "Ingresa la cantidad de dinero que desea convertir");
 
         try{
-            double num = Integer.parseInt(num1);
+            double num = Double.parseDouble(num1);
             SeleccionDivisa(frame, num);
             System.out.println(num);
         } catch(NullPointerException | NumberFormatException e){
@@ -22,6 +30,13 @@ public class ClaseConversor {
         }
     }
 
+    /**
+     * este metodo recibe dos parametros, el primero es frame paraa crear un nuevo cuadro y tambien un double n.
+     * Este metodo lo que contiene un array con todas las opciones que el usuario puede escoger.
+     * tambien este metodo llama a una nueva funcion para pasarle el numero y el la selecion.
+     * @param frame
+     * @param n
+     */
     public static void SeleccionDivisa(JFrame frame, double n){
         Object[] options = {
                 "De peso a Dolar",
@@ -40,11 +55,20 @@ public class ClaseConversor {
         resultado(frame, s, n);
     }
 
+    /**
+     * este metodo toma tres parametros el primero es tipo JFrame para crear el cuadro de dialogo
+     * el segundo es para la seleccion de divisa
+     * y el tercero el valor a convertir.
+     * tambien hay un flujo de condicion Switch el cual llama dos metodos
+     * De peso a otra divisa.
+     * De otra divisa a peso todo esto dependiendo la conversion que pidio el usuario
+     * @param frame
+     * @param s
+     * @param n
+     */
     public static void resultado(JFrame frame, String s, double n){
 
         String cond = s;
-        double result;
-
         switch (cond){
             case "De peso a Dolar":
                 operacion(frame, n, 0.017842225, "Dolares");
@@ -82,11 +106,24 @@ public class ClaseConversor {
 
     }
 
+    /**
+     * Este metodo contiene la operacion para calcular el resultado de peso a divisa
+     * @param frame
+     * @param n
+     * @param valor
+     * @param moneda
+     */
     private static void operacion(JFrame frame, double n, Double valor,String moneda){
         double result = n * valor;
         JOptionPane.showMessageDialog(frame, "El valor es: $" + Math.rint(result) + moneda, "Resultado", JOptionPane.PLAIN_MESSAGE);
     }
 
+    /**
+     * Este metodo contiene la operacion para calcular el resultado de la divisa al peso
+     * @param frame
+     * @param n
+     * @param valor
+     */
     private static void operaciondiv(JFrame frame, double n, Double valor){
         double result = n / valor;
         JOptionPane.showMessageDialog(frame, "El valor es: $" + Math.rint(result) + " pesos", "Resultado", JOptionPane.PLAIN_MESSAGE);
